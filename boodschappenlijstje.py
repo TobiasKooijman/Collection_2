c6fc7a9e82667f20ad0d9192e5a3c4e778050e7e
Tobias Kooijman <99068704@mydavinci.nl>
2021-12-06 11:19:07 +0100

    Joe Biden

--- boodschappenlijstje.py
+++ boodschappenlijstje.py
@@ -1,25 +1,11 @@
-vraag = "bruh"
-vraag2 = "joe Biden"
-lijst = {}
-print("--------------------------")
-print("type (klaar) als je klaar bent!")
-print("")
-vraag = str(input('Wat wil je bestellen? '))
-vraag2 = int(input('Hoeveel wil je daarvan bestellen? '))
-
-if vraag != "klaar":
-        lijst[vraag] = vraag2
-while vraag != "klaar":
-    vraag = str(input('Wat wil je nog meer bestellen? '))
-    if vraag == "klaar":
-        break
-    vraag2 = int(input('Hoeveel wil je daarvan bestellen? '))
-    if vraag2 == "klaar":
-        break
-    if vraag in lijst.keys():
-        lijst[vraag] = lijst[vraag] + vraag2
-    if vraag not in lijst.keys():
-        lijst[vraag] = vraag2
-print("")
-print("Boodschappenlijstje: ")
-print(lijst)
+import random
+kaarten = ['kaart 1', 'kaart 2', 'kaart 3','kaart 4', 'kaart 5','kaart 6', 'kaart 7']
+klaveren=['klaveren koning','klaveren vrouw', 'klaveren boer', 'klaveren 10','klaveren 9','klaveren 8','klaveren 7','klaveren 6','klaveren 5','klaveren 4','klaveren 3','klaveren 2']
+harten=['harten koning','harten vrouw','harten boer', 'harten 10','harten 9','harten 8','harten 7','harten 6','harten 5','harten 4','harten 3','harten 2']
+ruiten=['ruiten koning', 'ruiten vrouw', 'ruiten boer','ruiten 10','ruiten 9','ruiten 8','ruiten 7','ruiten 6','ruiten 5','ruiten 4','ruiten 3','ruiten 2']
+schoppen=['schoppen koning','schoppen vrouw','schoppen boer','schoppen 10','schoppen 9','schoppen 8','schoppen 7','schoppen 6','schoppen 5','schoppen 4', 'schoppen 3', 'schoppen 2']
+deck = klaveren + harten + ruiten + schoppen + ['Joker','Joker']
+for A, B in zip(kaarten, random.choices(deck, k = 7)):
+    print(A+":",B)
+    deck.remove(B)
+print('Deck[47 Kaarten]: ',deck)
--- kaarten_opdr.py
+++ kaarten_opdr.py
@@ -1,8 +1,29 @@
 import random
-from random import randrange
-kleuren = {"ruiten" , "klaveren" , "schoppen" , "ruiten"}
-for i in range(0,10):
-    kleuren["ruiten " + str(i)] = None
-for i in range(0,10):
-    kleuren["klaveren " + str(i)] = None
-print(kleuren)
+kleuren = ["ruiten" , "klaveren" , "schoppen" , "ruiten","Joker","Joker"]
+cijfers = ["aas","2","3","4","5","6","7","8","9","10","vrouw","boer","koning"]
+joebiden=[]
+trump=[]
+j =0
+def aghmmeeetrijkeeren():
+    global z
+    x = random.choice(kleuren)
+    if x != "Joker":
+        y = random.choice(cijfers)
+    else:
+        y = ""
+    z = x+" "+y
+    return(z,y,x)
+while j in range(0,1):
+    aghmmeeetrijkeeren()
+    if z not in joebiden or trump:
+        for t in range (0,7):
+            aghmmeeetrijkeeren()
+            joebiden.append(z)
+            t +=1
+        for t in range(8,54):
+            aghmmeeetrijkeeren()
+            trump.append(z)
+            t+=1
+        j +=1
+print("Gekozen Kaarten " + joebiden)
+print("Deck: " + trump)
